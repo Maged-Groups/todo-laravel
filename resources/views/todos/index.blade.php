bcff6e7f69fd219791bf7868e4fa03d07553cbe0
Maged <magedyaseen@hotmail.com>
2024-10-17 15:20:02 +0300

    View Created

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+
+	<title>Laravel</title>
+
+	<!-- Fonts -->
+	<link href="https://fonts.bunny.net" rel="preconnect">
+	<link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+	@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+	{{-- fontawesome 6 --}}
+	<script src="https://kit.fontawesome.com/b4a141d0bd.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="h-screen p-4 font-sans antialiased dark:bg-black dark:text-white/50 flex justify-center items-center">
+
+	<div class="h-full border shadow-md rounded-md pa-8 bg-white">
+
+	</div>
+</body>
+
+</html>
